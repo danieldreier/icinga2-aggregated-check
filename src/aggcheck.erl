6869a39c1b8a4b32163e1cb9430bbda3e7c11f2f
Daniel Dreier <d@puppetlabs.com>
2016-03-22 09:59:12 -0700

    refactor aggcheck to simplify state counts

--- src/aggcheck.erl
+++ src/aggcheck.erl
@@ -1,10 +1,11 @@
 -module(aggcheck).
--export([query_icinga/1, start/2, count_checks/2]).
+-export([query_icinga/1, start/2, count_checks/1]).
+-compile([export_all]).
 
--define(OK, 0).
--define(WARNING, 1).
--define(CRITICAL, 2).
--define(UNKNOWN, 3).
+-define(OK, 0.0).
+-define(WARNING, 1.0).
+-define(CRITICAL, 2.0).
+-define(UNKNOWN, 3.0).
 
 start(_Type, _Args) ->
     {ok,self()}.
@@ -25,6 +26,10 @@ setup_ssl([App|Apps]) ->
   setup_ssl(Apps),
   ok.
 
+auth_header({username,Username,password,Password}) ->
+  { "Authorization", "Basic " ++ base64:encode_to_string(Username ++ ":" ++ Password) }.
+
+% this appears to work as of March 20th
 query_icinga(Options) ->
   ok = setup_ssl(),
 
@@ -34,39 +39,83 @@ query_icinga(Options) ->
   Host = aggcheck_cli:setting(host, Options),
   Port = aggcheck_cli:setting(port, Options),
   Url = "https://" ++ Host ++ ":" ++ integer_to_list(Port) ++ "/v1/objects/services",
-  %io:format("URL: ~p", [Url]),
-  %PostBody = "{}",
   io:format("\n\nservice_filter: ~p\n\n", [list_to_binary(ServiceFilter)]),
-  %PostBody = "{ \"type\": \"Host\", \"filter\": \"\\\"role::elasticsearch::data\\\" in host.groups  && service.name == \\\"disk\\\"\" }",
-  %io:format(jsx:prettify(jsx:encode([{type,['Host', filter, [<<"\"role::elasticsearch\" in host.groups && service.name == \"disk\"">>]]}]))),
-  %io:format(jsx:prettify(jsx:encode([{type,['Host', filter, [list_to_binary(ServiceFilter)]]}]))),
-  %PostBody = "{ \"type\": \"Host\", \"filter\": \"" ++ ServiceFilter ++ "\" }",
-  %TestJson = jsx:encode({type, {'Host', filter, {ServiceFilter}}}),
   PostBody = jsx:encode([{<<"type">>,<<"Host">>},{<<"filter">>,list_to_binary(ServiceFilter)}]),
   io:format("\n\nPostBody: ~p\n\n", [PostBody]),
   PostContentType = "application/json",
   Response = httpc:request(post, {Url, [auth_header({username,Username,password,Password}),
                                         {"X-HTTP-Method-Override","GET"}], PostContentType, PostBody},
                                       [{ssl, [{verify, verify_none}]}], []),
-
-  %unknown(Response).
   case Response of
 %    {error, {failed_connect, _}} -> Body = undef, Headers = undef, nagios:unknown("SSL error");
     {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} -> io:format("something worked, got headers ~p\n\n", [Headers]);
     {ok, {{"HTTP/1.1",404,"Not Found"}, _      , _   }} -> Body = [], Headers = [], io:format("404, no results found")
     end,
-  io:format("response headers: ~p\n\n", [Headers]),
-  io:format("body: ~p\n\n", [binary:list_to_bin(Body)]),
-  jsx:decode(binary:list_to_bin(Body)).
-%%
-%
-count_checks(IcingaResponse, Options) ->
-  ok.
+  %io:format("response headers: ~p\n\n", [Headers]),
+  %io:format("body: ~p\n\n", [binary:list_to_bin(Body)]),
+  [{<<"results">>,ExtractedResults}] = jsx:decode(binary:list_to_bin(Body)),
+  ExtractedResults.
 
-auth_header({username,Username,password,Password}) ->
-% ruby version:
-% auth = 'Basic ' + Base64.encode64("#{config[:username]}:#{config[:password]}").chomp
-% auth_headers = { 'Authorization' => auth }
-  { "Authorization", "Basic " ++ base64:encode_to_string(Username ++ ":" ++ Password) }.
+munge_icinga_results(IcingaResponse) ->
+  % convert the icinga results into a format that's easier for us to work with
+  % specifically, let's make it just a list of lists and remove the {<<"attrs">> bit
+  lists:map( fun(X) -> {<<"attrs">>, Record} = lists:nth(1, X), Record end, IcingaResponse).
 
+% filter Icinga API response (after flattening by munge_icinga_results)
+% returns a list in the same format, but only containing results for checks
+% in the state indicated by the State parameter. State param must be a float
+% (1.0, 2.0) because of how the API response is decoded.
+filter_results_by_state(ResultList, State) ->
+  lists:filter(fun (X) ->
+    case lists:keyfind(<<"last_hard_state">>, 1, X) of
+        {<<"last_hard_state">>,State} -> Return = true;
+        {<<"last_hard_state">>,_}     -> Return = false
+    end,
+    Return end,
+  ResultList).
+
+% select checks with active set to true
+filter_active_results(ResultList) ->
+  lists:filter(fun (X) ->
+    case lists:keyfind(<<"active">>, 1, X) of
+        {<<"active">>,true} -> Return = true;
+        false               -> Return = false
+    end,
+    Return end,
+  ResultList).
+
+% extended check output will list which hosts matched, and what the display
+% name and last hard state of the matched check was. This should make it
+% faster to determine which backend host(s) and check(s) to investigate.
+getHostNames(ResultList) ->
+  lists:map(fun (X) ->
+    {<<"host_name">>,       HostName}         = lists:keyfind(<<"host_name">>,       1, X),
+    {<<"display_name">>,    CheckDisplayName} = lists:keyfind(<<"display_name">>,    1, X),
+    {<<"last_hard_state">>, CheckHardState}   = lists:keyfind(<<"last_hard_state">>, 1, X),
+    [{host, HostName}, {check, CheckDisplayName}, {check_hard_state, CheckHardState}] end,
+  ResultList).
+
+count_results_by_state(ResultList, State) ->
+  length(filter_results_by_state(ResultList, State)).
+
+count_checks(ResultList) ->
+  % we only care about active (not disabled) checks, so filter for those first
+  % after this, we only look at ActiveChecks not the full ResultList
+  ActiveChecks = filter_active_results(ResultList),
+
+  % find results in "OK" state
+  OkCount = count_results_by_state(ResultList, ?OK),
+
+  % find results in "warning" state
+  WarnCount = count_results_by_state(ResultList, ?WARNING),
+
+  % find results in "critical" state
+  CritCount = count_results_by_state(ResultList, ?CRITICAL),
+
+  % find results in "unknown" state
+  UnknownCount = count_results_by_state(ResultList, ?UNKNOWN),
+  % count total number of active checks
+  TotalCount = length(ActiveChecks),
+
+  {ok, {total, TotalCount}, {ok, OkCount}, {warning, WarnCount}, {critical, CritCount}, {unknown, UnknownCount}}.
 
